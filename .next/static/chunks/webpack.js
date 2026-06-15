7862e7cc52ea9d50de48a4c5cd64e31c1e1dc7d2
Harsh Yadav <harsh.yadav@hbwsl.com>
2026-06-15 17:56:33 +0530

    chore: update Next.js build artifacts and manifest files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e4820c8895c6bf16"; }
+/******/ 		__webpack_require__.h = function() { return "08d92c1e0932b83a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
